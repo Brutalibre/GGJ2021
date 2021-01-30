e8c6705cf7957fd0ae490f63e70a7ee0fda4d3f7
Togi <ThomasGiro@aol.com>
2021-01-30 15:30:46 +0100

    Arm in FBX

--- Assets/Scripts/ArmController.cs
+++ Assets/Scripts/ArmController.cs
@@ -8,17 +8,18 @@ public class ArmController : MonoBehaviour {
     public Camera main;
     public BoxCollider plane;
     public float planeDistance;
+    public float scalingDest = 4;
 
     void FixedUpdate() {
         Ray ray = main.ScreenPointToRay(Input.mousePosition);
         if (plane.Raycast(ray, out RaycastHit pointHit, planeDistance)) {
-            Vector3 destination = new Vector3(pointHit.point.x, body.transform.position.y, pointHit.point.z);
+            Vector3 destination = new Vector3(pointHit.point.x * scalingDest, body.transform.position.y, pointHit.point.z * scalingDest);
             Vector3 direction = (destination - body.transform.position) * sensitivity;
 
             body.velocity = direction;
-            body.angularVelocity = new Vector3 ((-direction.x) / body.inertiaTensor.x,
+            /*body.angularVelocity = new Vector3 ((-direction.x) / body.inertiaTensor.x,
                                                 (-direction.y) / body.inertiaTensor.y,
-                                                (-direction.z) / body.inertiaTensor.z);
+                                                (-direction.z) / body.inertiaTensor.z);*/
         }
     }
 }
