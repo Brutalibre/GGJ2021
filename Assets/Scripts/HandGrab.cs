4a9dfe089a2f1528614cc11849295900dcd04196
Val Isautier <val.isautier@gmail.com>
2021-01-31 11:38:53 +0100

    Hand grabs!

--- Assets/Scripts/HandGrab.cs
+++ Assets/Scripts/HandGrab.cs
@@ -7,26 +7,38 @@ public class HandGrab : MonoBehaviour
     public Collider handTrigger;
     private Grabbable grabbed = null;
     private Collider inRange = null;
+    private bool areAllFingersClosed = false;
     public Transform GrabbedObjectDestination;
 
     public List<GameObject> fingers;
     List<Quaternion> fingersQuat = null;
+    List<bool> fingersClosed = null;
 
     private void Awake() {
         fingersQuat = new List<Quaternion>();
         for (int i = 0; i < fingers.Count; i++) {
             fingersQuat.Add(fingers[i].transform.localRotation);
         }
+
+        fingersClosed = new List<bool>();
+        for (int i = 0; i < fingers.Count/2; i++) {
+            fingersClosed.Add(false);
+        }
     }
 
     float tThumbs, tIndex, tMiddle, tRing, tPinkie;
 
     private void Update() {
-        if (Input.GetKey(KeyCode.Space)) tThumbs += Time.deltaTime * 2; else tThumbs -= Time.deltaTime * 4;
-        if (Input.GetKey(KeyCode.R)) tIndex += Time.deltaTime * 2; else tIndex -= Time.deltaTime * 4;
-        if (Input.GetKey(KeyCode.E)) tMiddle += Time.deltaTime * 2; else tMiddle -= Time.deltaTime * 4;
-        if (Input.GetKey(KeyCode.Z)) tRing += Time.deltaTime * 2; else tRing -= Time.deltaTime * 4;
-        if (Input.GetKey(KeyCode.A)) tPinkie += Time.deltaTime * 2; else tPinkie -= Time.deltaTime * 4;
+        if (Input.GetKey(KeyCode.Space)) { fingersClosed[0] = true;  tThumbs += Time.deltaTime * 2; }
+                                    else { fingersClosed[0] = false; tThumbs -= Time.deltaTime * 4; }
+        if (Input.GetKey(KeyCode.R)) { fingersClosed[1] = true;  tIndex += Time.deltaTime * 2; }
+                                else { fingersClosed[1] = false; tIndex -= Time.deltaTime * 4; }
+        if (Input.GetKey(KeyCode.E)) { fingersClosed[2] = true;  tMiddle += Time.deltaTime * 2; }
+                                else { fingersClosed[2] = false; tMiddle -= Time.deltaTime * 4; }
+        if (Input.GetKey(KeyCode.Z)) { fingersClosed[3] = true;  tRing += Time.deltaTime * 2; }
+                                else { fingersClosed[3] = false; tRing -= Time.deltaTime * 4; }
+        if (Input.GetKey(KeyCode.A)) { fingersClosed[4] = true;  tPinkie += Time.deltaTime * 2; }
+                                else { fingersClosed[4] = false; tPinkie -= Time.deltaTime * 4; }
         tThumbs = Mathf.Clamp(tThumbs, 0, 1);
         tIndex = Mathf.Clamp(tIndex, 0, 1);
         tMiddle = Mathf.Clamp(tMiddle, 0, 1);
@@ -38,7 +50,29 @@ public class HandGrab : MonoBehaviour
         CloseFinger(3, tRing);
         CloseFinger(4, tPinkie);
 
-        if (Input.GetKeyDown(KeyCode.A)) {
+        // Is hand fully closed?
+        if (AreAllFingersClosed()) {
+            // If it is, does it already hold something ?
+            // If not, is there something in range?
+            if (grabbed == null && inRange != null) {
+                // If it is, grab this thing !
+                grabbed = inRange.GetComponent<Grabbable>();
+                grabbed.SetGrabbedBy(this);
+            }
+            // If it is, DO NOTHING
+        }
+        else {
+            // If not, does it already hold something ?
+            if (grabbed != null) {
+                // If it is, ungrab !
+                grabbed.Ungrab();
+                grabbed = null;
+            }
+            // If not, DO NOTHING
+        }
+
+
+        /*if (AreAllFingersClosed()) {
             // Is the hand holding something ?
             // Let go of the grabbed object.
             if (grabbed != null) {
@@ -52,12 +86,12 @@ public class HandGrab : MonoBehaviour
                     grabbed = inRange.GetComponent<Grabbable>();
                     grabbed.SetGrabbedBy(this);
                 }
-                // Close/open the hand.
+                // Do nothing !
                 else {
 
                 }
             }
-        }
+        }*/
     }
 
     public void CloseThumbs(float t) {
@@ -72,6 +106,17 @@ public class HandGrab : MonoBehaviour
             = Quaternion.Lerp(fingersQuat[i * 2], fingersQuat[i * 2] * Quaternion.Euler(0, 0, 95), t);
         fingers[i * 2+ 1].transform.localRotation
             = Quaternion.Lerp(fingersQuat[i*2+1], fingersQuat[i*2+1] * Quaternion.Euler(0, 0, -75), t);
+    }
+
+    public bool AreAllFingersClosed() {
+        bool closed = true;
+
+        foreach (bool finger in fingersClosed) {
+            closed = closed && finger;
+        }
+
+        Debug.Log("closed = " + closed);
+        return closed;
     }
 
     void OnTriggerEnter(Collider other) {
