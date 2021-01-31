8cd5cd84b7494365473b92b5a4746a79863538b4
Togi <ThomasGiro@aol.com>
2021-01-31 15:07:11 +0100

    Add audio script

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,33 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioIntroLoop : MonoBehaviour {
+public class AudioManager : MonoBehaviour {
     public AudioSource audioSourceMenu;
     public AudioSource audioSourceGame;
     public AudioClip musicIntro;
     
     public bool inMenu = true;
-    static bool instancied = false;
+    public static AudioManager instance = null;
 
     void Start() {
-        if (instancied) Destroy(this.gameObject);
-        instancied = true;
+        if (instance) Destroy(this.gameObject);
+        instance = this;
         DontDestroyOnLoad(this.gameObject);
 
         audioSourceMenu.enabled = true;
         audioSourceGame.enabled = false;
     }
 
-    public bool debug;
-    public void Update() {
-        if (debug)
-            ChangeScene(!inMenu);
-
-        debug = false;
-    }
-
     public void ChangeScene(bool menu) {
-        if(menu) {
+        if (inMenu == menu) return;
+        if (menu) {
             audioSourceMenu.enabled = true;
             audioSourceGame.enabled = false;
         } else {
