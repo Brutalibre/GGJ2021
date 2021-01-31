8949f58edcb30656885d371a22407fe0754a7318
Togi <ThomasGiro@aol.com>
2021-01-31 15:00:05 +0100

    Add control music title/ingame

--- Assets/Scripts/AudioIntroLoop.cs
+++ Assets/Scripts/AudioIntroLoop.cs
@@ -3,18 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioIntroLoop : MonoBehaviour {
-    public AudioSource audioSource;
+    public AudioSource audioSourceMenu;
+    public AudioSource audioSourceGame;
     public AudioClip musicIntro;
-
-    public bool launchGame = false;
+    
+    public bool inMenu = true;
+    static bool instancied = false;
 
     void Start() {
-        audioSource.PlayOneShot(musicIntro);
-        audioSource.PlayScheduled(AudioSettings.dspTime + musicIntro.length);
-        //audioSource.timeSamples
+        if (instancied) Destroy(this.gameObject);
+        instancied = true;
+        DontDestroyOnLoad(this.gameObject);
+
+        audioSourceMenu.enabled = true;
+        audioSourceGame.enabled = false;
     }
 
-    private void Update() {
-        
+    public bool debug;
+    public void Update() {
+        if (debug)
+            ChangeScene(!inMenu);
+
+        debug = false;
+    }
+
+    public void ChangeScene(bool menu) {
+        if(menu) {
+            audioSourceMenu.enabled = true;
+            audioSourceGame.enabled = false;
+        } else {
+            audioSourceMenu.enabled = false;
+            audioSourceGame.enabled = true;
+            audioSourceGame.PlayOneShot(musicIntro);
+            audioSourceGame.PlayScheduled(AudioSettings.dspTime + musicIntro.length);
+        }
+        inMenu = menu;
     }
 }
